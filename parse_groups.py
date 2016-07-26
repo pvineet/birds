7b4f13ca72176484559c412bf1fd606faed17dd5
pvineet <pvineet@gmail.com>
2016-07-27 00:05:07 +0530

    parse the bird groups

---
+++ parse_groups.py
@@ -0,0 +1,32 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+base_url = "http://orientalbirdimages.org"
+bird_images_url = "http://orientalbirdimages.org/birdimages.php"
+groups = []
+
+def parse_images_page(response):
+    page_content = BeautifulSoup(response.content, 'html.parser')
+    with open('groups.csv', 'wb') as csvfile:
+        fieldnames = ['group_name', 'href']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for link in page_content.find_all('a'):
+            #print link.__dict__
+            try:
+                if(link.get('class')[0] == 'mlink'):
+                    href = link.get('href')
+                    writer.writerow({'group_name': link.next_element, 'href': base_url+href})
+            except TypeError:
+                print "Not the correct link"
+    return 0
+
+response = requests.get(bird_images_url)
+print type(response.status_code)
+if(response.status_code == 200):
+    print "Page available"
+else:
+    print "Error in reaching bird images page"
+
+parse_images_page(response)

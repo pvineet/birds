e47bf8694967b8b8c2c1c018ce5277b098cca021
pvineet <pvinet@gmail.com>
2016-09-10 09:29:04 +0000

    changes to models

--- display/admin.py
+++ display/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Group, GroupAdmin)
--- display/models.py
+++ display/models.py
@@ -3,5 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Bird(models.Model):
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+class Family(models.Model):
+    name = models.CharField(max_length=200)
+
+class Specie(models.Model):
     name = models.CharField(max_length=200)
